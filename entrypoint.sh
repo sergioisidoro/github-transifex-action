fbc1ec81c029fb40a81966d3b2297357562f177b
Sérgio <smaisidoro@gmail.com>
2020-01-30 17:36:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh -l
 
-tx init --no-interactive
-tx push -s 
+config= <<EOF
+[https://www.transifex.com]
+api_hostname = https://api.transifex.com
+hostname = https://www.transifex.com
+password = $TX_TOKEN
+username = api
+)
+EOF
+
+echo $config > ~/.transifexrc
+
+tx push -s
