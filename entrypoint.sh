8c9afb2fd95405073177d6bd38dfcf44439f3d13
Sérgio <smaisidoro@gmail.com>
2020-02-28 09:58:28 +0200

    Fix missing quotes on translations pulling

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo $config > ~/.transifexrc
 
-if [    "$PUSH_SOURCES" = true ] ; then
+if [ "$PUSH_SOURCES" = true ] ; then
     echo "PUSHING SOURCES"
     tx push -s
 fi
@@ -17,7 +17,7 @@ if [ "$PULL_SOURCES" = true ] ; then
     tx pull -s
 fi
 
-if [ $PULL_TRANSLATIONS = true ] ; then
+if [ "$PULL_TRANSLATIONS" = true ] ; then
     echo "PULLING TRANSLATIONS"
     tx pull -a
 fi
