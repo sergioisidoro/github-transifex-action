06c646fb0d9df07b5ad559db9e25cd9df2bff635
sergioisidoro <sergio.isidoro@kaikuhealth.com>
2020-03-12 15:05:20 +0200

    Parallel and fixes to git flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ MASTER_BRANCH=${INPUT_MASTER_BRANCH:-master}
 REMOTE=${INPUT_REMOTE:-origin}
 TRANSLATIONS_FOLDER=${INPUT_TRANSLATIONS_FOLDER}
 SKIP_PUSH_COMMIT=${INPUT_SKIP_PUSH_COMMIT}
+
+COMMITER_EMAIL=${INPUT_COMMITER_EMAIL:-'git-action@transifex.com'}
+COMMITTER_NAME=${INPUT_COMMITTER_NAME:-'Transifex Github action'}
+
 export TX_TOKEN=${INPUT_TX_TOKEN:-$TX_TOKEN}
 
 # Because transifex overrides whatever is in remote with what's currently in
@@ -21,6 +25,11 @@ export TX_TOKEN=${INPUT_TX_TOKEN:-$TX_TOKEN}
 # 5 - Push the merge result to transifex
 
 args=()
+common_args=()
+
+if [[ $INPUT_PARALLEL ]] ; then
+    common_args+=( "--parallel" )
+fi
 
 if [[ $INPUT_MINIMUM_PERC -ne 0 ]] ; then
     args+=( "--minimum-perc=$INPUT_MINIMUM_PERC" )
@@ -40,8 +49,8 @@ if [[ $INPUT_GIT_FLOW ]]; then
     };
 
     echo "Setting up git repo"
-    git config --global user.email "${INPUT_COMMITTER_EMAIL}"
-    git config --global user.name "${INPUT_COMMITTER_NAME}"
+    git config --global user.email "${COMMITTER_EMAIL}"
+    git config --global user.name "${COMMITTER_NAME}"
     remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
     git fetch --all
 
@@ -49,7 +58,7 @@ if [[ $INPUT_GIT_FLOW ]]; then
     TRANSLATIONS_MERGE_BRANCH="${MASTER_BRANCH}-translations-$(date +%s)"
     git checkout -b ${TRANSLATIONS_MERGE_BRANCH}
     echo "Pulling most up to date sources"
-    tx pull -a --no-interactive
+    tx pull -a --no-interactive "${common_args[@]}"
 
     # Merges the current branch to the most up to date translations
     git add "${TRANSLATIONS_FOLDER}"
@@ -58,26 +67,28 @@ if [[ $INPUT_GIT_FLOW ]]; then
     # and let's push the merged version upstream
 
     if [[ $INPUT_PUSH_SOURCES ]] && [[ $INPUT_PUSH_TRANSLATIONS ]] ; then
-        tx push -s --no-interactive
+        tx push -s --no-interactive "${common_args[@]}"
     else
         if [[ $INPUT_PUSH_SOURCES ]] ; then
-            tx push -s --no-interactive
+            tx push -s --no-interactive "${common_args[@]}"
         fi
 
         if [[ $INPUT_PUSH_TRANSLATIONS ]] ; then
-            tx push -t --no-interactive
+            tx push -t --no-interactive "${common_args[@]}"
         fi
     fi
 
     if [[ $INPUT_PULL_SOURCES ]] ; then
-        tx pull -s --no-interactive "${args[@]}"
+        tx pull -s --no-interactive "${args[@]}" "${common_args[@]}"
     fi
 
     if [[ $INPUT_PULL_TRANSLATIONS ]] ; then
-        tx pull -a --no-interactive "${args[@]}"
+        tx pull -a --no-interactive "${args[@]}" "${common_args[@]}"
     fi
 
     if [[ $INPUT_PULL_SOURCES ]] || [[ $INPUT_PULL_TRANSLATIONS ]] ; then
+        # Stashes all of the non needed changes (eg. sometines .tx/config is changed)
+        git stash
         git checkout $CURRENT_BRANCH
         git checkout ${TRANSLATIONS_MERGE_BRANCH} -- $TRANSLATIONS_FOLDER
         git add "${TRANSLATIONS_FOLDER}"
@@ -93,21 +104,21 @@ else
     echo "USING OVERRIDE FLOW"
     if [[ $INPUT_PULL_SOURCES ]] ; then
         echo "PULLING SOURCES"
-        tx pull -s --no-interactive
+        tx pull -s --no-interactive "${common_args[@]}"
     fi
 
     if [[ $INPUT_PULL_TRANSLATIONS ]] ; then
         echo "PULLING TRANSLATIONS (with args: $args)"
-        tx pull -a --no-interactive "${args[@]}"
+        tx pull -a --no-interactive "${args[@]}" "${common_args[@]}"
     fi
 
     if [[ $INPUT_PUSH_SOURCES ]] ; then
         echo "PUSHING SOURCES"
-        tx push -s --no-interactive
+        tx push -s --no-interactive "${common_args[@]}"
     fi
 
     if [[ $INPUT_PUSH_TRANSLATIONS ]] ; then
         echo "PUSHING TRANSLATIONS"
-        tx push -t --no-interactive
+        tx push -t --no-interactive "${common_args[@]}"
     fi
 fi
