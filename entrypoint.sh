96d33e6c4d0153a6942935a613ea4f56915679a4
Matias Berglund <matias.berglund@hotmail.com>
2022-01-19 13:53:11 +0200

    Added possibility to pull translations of specific resource

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,8 +72,11 @@ if [[ "$INPUT_GIT_FLOW" = true ]] ; then
     git checkout origin/${MASTER_BRANCH}
     TRANSLATIONS_MERGE_BRANCH="${MASTER_BRANCH}-translations-$(date +%s)"
     git checkout -b ${TRANSLATIONS_MERGE_BRANCH}
-
-    if [[ "$INPUT_PULL_SOURCES" = true ]] || [[ "$INPUT_PULL_TRANSLATIONS" = true ]] ; then
+    
+    if ( ([[ "$INPUT_PULL_SOURCES" = true ]] || [[ "$INPUT_PULL_TRANSLATIONS" = true ]]) && [[ "$INPUT_SPECIFY_RESOURCE" = true ]] ) ; then
+        echo "Pulling most up to date sources and translations with the specified resource: $INPUT_PROJECT.$INPUT_RESOURCE"
+        tx pull --no-interactive --force "${common_args[@]}" "${args[@]}" -r "$INPUT_PROJECT"."$INPUT_RESOURCE"
+    elif [[ "$INPUT_PULL_SOURCES" = true ]] || [[ "$INPUT_PULL_TRANSLATIONS" = true ]] ; then
         echo "Pulling most up to date sources and translations"
         # Unfortunately we need to use force because transifex thinks the checked out
         # files are newer than in Transifex, so they get ignored. See issue:
@@ -102,7 +105,7 @@ if [[ "$INPUT_GIT_FLOW" = true ]] ; then
     fi
 
     if [[ "$INPUT_COMMIT_TO_PR" = true ]] ; then
-        # Stashes all of the non needed changes (eg. sometines .tx/config is changed)
+        # Stashes all of the non needed changes (eg. sometimes .tx/config is changed)
         git stash
         git checkout $CURRENT_BRANCH
         git checkout ${TRANSLATIONS_MERGE_BRANCH} -- $TRANSLATIONS_FOLDER
